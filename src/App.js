88b1648a324e8e50d2170ce2f29430b4f22f0110
zephyr664 <dbrgamei5@gmail.com>
2022-08-19 17:21:21 -0300

    screen login (req 2, 3, 4, 5, 6)

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './pages/Login/Login';
 
 function App() {
   return (
     <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={ Login } exact />
+          <Route path="/foods" exact />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
