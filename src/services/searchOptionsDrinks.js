d0578b44e61647bb80a4d85e4917dfc5685dee75
Miyuki Ishii <miyukisabrina@gmail.com>
2022-08-20 20:49:01 -0300

    última versão

--- src/services/searchOptionsDrinks.js
+++ src/services/searchOptionsDrinks.js
@@ -1,20 +1,32 @@
 export async function fetchRecipesByIngredient(ingredient) {
-  const endpoint = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`;
-  const data = await fetch(endpoint)
-    .then((response) => response.json());
-  return data.drinks;
+  try {
+    const endpoint = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`;
+    const data = await fetch(endpoint)
+      .then((response) => response.json());
+    return data.drinks;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export async function fetchRecipesByLetter(letter) {
-  const endpoint = `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${letter}`;
-  const data = await fetch(endpoint)
-    .then((response) => response.json());
-  return data.drinks;
+  try {
+    const endpoint = `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${letter}`;
+    const data = await fetch(endpoint)
+      .then((response) => response.json());
+    return data.drinks;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export async function fetchRecipesByName(name) {
-  const endpoint = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`;
-  const data = await fetch(endpoint)
-    .then((response) => response.json());
-  return data.drinks;
+  try {
+    const endpoint = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`;
+    const data = await fetch(endpoint)
+      .then((response) => response.json());
+    return data.drinks;
+  } catch (error) {
+    console.log(error);
+  }
 }
--- src/services/searchOptionsFoods.js
+++ src/services/searchOptionsFoods.js
@@ -1,20 +1,32 @@
 export async function fetchRecipesByIngredient(ingredient) {
-  const endpoint = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`;
-  const data = await fetch(endpoint)
-    .then((response) => response.json());
-  return data.meals;
+  try {
+    const endpoint = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`;
+    const data = await fetch(endpoint)
+      .then((response) => response.json());
+    return data.meals;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export async function fetchRecipesByLetter(letter) {
-  const endpoint = `https://www.themealdb.com/api/json/v1/1/search.php?f=${letter}`;
-  const data = await fetch(endpoint)
-    .then((response) => response.json());
-  return data.meals;
+  try {
+    const endpoint = `https://www.themealdb.com/api/json/v1/1/search.php?f=${letter}`;
+    const data = await fetch(endpoint)
+      .then((response) => response.json());
+    return data.meals;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export async function fetchRecipesByName(name) {
-  const endpoint = `https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`;
-  const data = await fetch(endpoint)
-    .then((response) => response.json());
-  return data.meals;
+  try {
+    const endpoint = `https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`;
+    const data = await fetch(endpoint)
+      .then((response) => response.json());
+    return data.meals;
+  } catch (error) {
+    console.log(error);
+  }
 }
