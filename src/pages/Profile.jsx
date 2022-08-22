40d143daaebf4a61eef8324ed338393d7114969e
zephyr664 <dbrgamei5@gmail.com>
2022-08-22 17:30:15 -0300

    fix req 8

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -5,36 +5,51 @@ import Header from '../components/Header';
 
 function Profile() {
   const user = JSON.parse(localStorage.getItem('user'));
-
   const resetLocalStorage = () => {
     localStorage.clear();
   };
 
   return (
     <div>
-      <Header />
-      <h1>Profile</h1>
-      <span data-testid="profile-email">{user.email}</span>
-      <Link
-        to="/done-recipes"
-        data-testid="profile-done-btn"
-      >
-        Done Recipes
-      </Link>
-      <Link
-        to="/favorite-recipes"
-        data-testid="profile-favorite-btn"
-      >
-        Favorite Recipes
-      </Link>
-      <Link
-        to="/"
-        data-testid="profile-logout-btn"
-        onClick={ resetLocalStorage }
-      >
-        Logout
-      </Link>
-      <Footer />
+      {
+        (user)
+          ? (
+            <div>
+              <Header />
+              <h1>Profile</h1>
+              <span data-testid="profile-email">{user.email}</span>
+              <Link
+                to="/done-recipes"
+                data-testid="profile-done-btn"
+              >
+                Done Recipes
+              </Link>
+              <Link
+                to="/favorite-recipes"
+                data-testid="profile-favorite-btn"
+              >
+                Favorite Recipes
+              </Link>
+              <Link
+                to="/"
+                data-testid="profile-logout-btn"
+                onClick={ resetLocalStorage }
+              >
+                Logout
+              </Link>
+              <Footer />
+            </div>
+          )
+          : <div>
+            <Header />
+            <h1>Profile</h1>
+            <Link
+              to="/"
+            >
+              Logout
+            </Link>
+          </div>
+      }
     </div>
   );
 }
