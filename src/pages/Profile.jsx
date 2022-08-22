9d68236a20984387d47539d303b6b1b17cc600c9
zephyr664 <dbrgamei5@gmail.com>
2022-08-22 16:28:18 -0300

    req 57, 58, 59, 60, 61

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+
+function Profile() {
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  const resetLocalStorage = () => {
+    localStorage.clear();
+  };
+
+  return (
+    <div>
+      <Header />
+      <h1>Profile</h1>
+      <span data-testid="profile-email">{user.email}</span>
+      <Link
+        to="/done-recipes"
+        data-testid="profile-done-btn"
+      >
+        Done Recipes
+      </Link>
+      <Link
+        to="/favorite-recipes"
+        data-testid="profile-favorite-btn"
+      >
+        Favorite Recipes
+      </Link>
+      <Link
+        to="/"
+        data-testid="profile-logout-btn"
+        onClick={ resetLocalStorage }
+      >
+        Logout
+      </Link>
+      <Footer />
+    </div>
+  );
+}
+
+export default Profile;
