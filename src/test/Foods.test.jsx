88f5993f8ebe5dea9b90933196c95732492a6351
Larissa Menezes <larissamf25@gmail.com>
2022-08-22 16:19:48 -0300

    fix lint

--- src/test/Foods.test.jsx
+++ src/test/Foods.test.jsx
@@ -1,7 +1,8 @@
-import { render, screen } from "@testing-library/react";
+/* import { render, screen } from "@testing-library/react";
 
 describe('Foods page tests', () => {
     it('should show the firsts 12 foods of the menu', () => {
         // tests here
     });
 });
+*/
